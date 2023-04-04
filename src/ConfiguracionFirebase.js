18264196a04b03b65d4781d61659c94f9c9a0c7c
AngelGarciaCabrera <122624789+AngelGarciaCabrera@users.noreply.github.com>
2023-04-04 17:42:50 -0500

    Primer commit

--- src/ConfiguracionFirebase.js
+++ src/ConfiguracionFirebase.js
@@ -1,13 +1,15 @@
+import { initializeApp } from "firebase/app";
 import firebase from "firebase/app";
 import 'firebase/firestore';
 
-var firebaseConfig = {
-    apiKey: "AIzaSyB79SCj8reuOlPXf23D4lCqd8lJ_GmTOEo",
-    authDomain: "agenda-itla.firebaseapp.com",
-    projectId: "agenda-itla",
-    storageBucket: "agenda-itla.appspot.com",
-    messagingSenderId: "180764554754",
-    appId: "1:180764554754:web:a9550ec82b74832294591b"
+const firebaseConfig = {
+  apiKey: "AIzaSyDR0fy5yIn9hLSOoEbqX-q3hr4zMAU9A8M",
+  authDomain: "agenda-react-54e54.firebaseapp.com",
+  projectId: "agenda-react-54e54",
+  storageBucket: "agenda-react-54e54.appspot.com",
+  messagingSenderId: "462751414534",
+  appId: "1:462751414534:web:225998911f77a38e9a4687"
+   
   };
 
 firebase.initializeApp(firebaseConfig);
